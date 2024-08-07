9a810b0273f027dd3a7513fbcacddf71e200cb58
VIKTOR RENEV <easykillme@outlook.com>
2024-08-07 15:31:31 +0900

    测试MD文件位置是否可以正常显示

--- Assets/TankMovement.cs
+++ Assets/TankMovement.cs
@@ -18,17 +18,12 @@ public class TankMovement : MonoBehaviour
     }
     private void Move()
     {
+      
   
         float verticalInput = Input.GetAxis("Vertical");
-        Vector3 moveDirection = new Vector3(0, 0, verticalInput);
         if (verticalInput!=0)
         {
-
-            // 使用 TransformDirection 来获取正确的世界方向
-            Vector3 worldDirection = transform.TransformDirection(moveDirection);
-            transform.position += worldDirection * Time.deltaTime*speed;
-
-            //transform.Translate(transform.forward * verticalInput * Time.deltaTime * speed);
+            transform.Translate(transform.forward * verticalInput * Time.deltaTime * speed);
         }
        
     }
